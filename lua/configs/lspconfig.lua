208f18f3da98f8f81a7dd2e75e5496c37033638b
Gubian, Sylvain <Sylvain.Gubian@pmi.com>
2026-03-25 13:27:31 +0100

    Added pyright support

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,7 +3,6 @@ require("nvchad.configs.lspconfig").defaults()
 local servers = {
   "air",
   "lua_ls",
-  "pylsp",
   "julials",
   "marksman",
   "html",
@@ -11,4 +10,46 @@ local servers = {
 }
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+-- For python
+vim.lsp.config('ruff', {})
+vim.lsp.enable('ruff')
+
+vim.lsp.config('pyright', {
+  on_attach = function(client, _)
+    if client.server_capabilities.signatureHelpProvider then
+      client.server_capabilities.signatureHelpProvider.triggerCharacters = {}
+    end
+  end,
+  capabilities = capabilities,
+  settings = {
+    python = {
+      analysis = {
+        typeCheckingMode = "basic",
+      },
+    },
+  },
+})
+vim.lsp.enable('pyright')
+
+-- For Rust
+vim.lsp.config("rust_analyzer", {
+  cmd = { "rust-analyzer" },
+  filetypes = { "rust" },
+  root_markers = { "Cargo.toml", "rust-project.json" },
+  on_attach = function(client, _)
+    -- Kill automatic signature help triggers
+    if client.server_capabilities.signatureHelpProvider then
+      client.server_capabilities.signatureHelpProvider.triggerCharacters = {}
+    end
+  end,
+})
+vim.lsp.handlers["textDocument/signatureHelp"] = function(err, result, ctx, config)
+  if not result or not result.signatures or #result.signatures == 0 then return end
+  vim.lsp.handlers["textDocument/signatureHelp"](err, result, ctx,
+    vim.tbl_extend("force", config or {}, { border = "rounded" }))
+end
+
+vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "LSP hover" })
+
